9144370048d39cf88609c8b888ca4af81438ec4d
piotsik <36536181+piotsik@users.noreply.github.com>
2018-10-16 11:47:43 +0200

    Initial Commit

---
+++ cp3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cp3
+//
+//  Created by Piotr Sikora on 16/10/2018.
+//  Copyright © 2018 Piotr Sikora. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
